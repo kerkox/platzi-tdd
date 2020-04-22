1ccd3e08485727324aa8101ac854fbf94c13f828
Paul Cortes <paulkerg@gmail.com>
2020-04-21 21:45:26 -0500

    [Add] method test_post_delete

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -87,4 +87,17 @@ class PostControllerTest extends TestCase
 
         $this->assertDatabaseMissing('posts', ['id' => $post->id]);
     }
+
+    public function test_post_index()
+    {
+        factory(Post::class, 5)->create();
+
+        $response = $this->json('GET', '/api/posts');
+
+        $response->assertJsonStructure([
+           'data' => [
+               '*' => ['id', 'title', 'created_at', 'updated_at']
+           ]
+        ])->assertStatus(200);
+    }
 }
