050cb9d09fc938f0e8a8865b747c35fdec8e981b
Paul Cortes <paulkerg@gmail.com>
2020-04-21 19:59:46 -0500

    creacion del metodo test_store en la prueba de PostControllerTest

--- tests/Feature/Http/Controllers/Api/PostControllerTest.php
+++ tests/Feature/Http/Controllers/Api/PostControllerTest.php
@@ -8,15 +8,24 @@ use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function testExample()
+    public function test_store()
     {
-        $response = $this->get('/');
+        $response = $this->json('POST','/api/posts', [
+            'title' => 'El post de prueba'
+            ]);
 
-        $response->assertStatus(200);
+        $response->assertJsonStructure(['id', 'title', 'created_at', 'updated_at'])
+        ->assertJson(['title' => 'El post de prueba'])
+        ->assertStatus(201); //Ok, creado un recurso
+
+        $this->assertDatabaseHas('posts', ['title' => 'El post de prueba']);
     }
 }
